5cb2bf45b74856d106e953b1513c31a0b4055121
Don-V <donv.amupitan@icloud.com>
2017-02-12 02:38:41 -0600

    Refactored board component to game component, adds to #4, fixes #6

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,96 +18,53 @@ var shuffle = arr => {
   return array;
 };
 
-class Square extends React.Component{
-  
-   constructor() {
-    super();
-    this.state = {
-      value: initial++, //Populate the values
-    };
-      console.log("Puzzle: " + puzzle[puzzle.length-1]); //Always 9 {Actually the console always logs 8}
-      // squareLocation.push(this);
-    }
-
-
-  output(c){
-    //Updates puzzle array
-    var swap = function(a, b){
-      moves++;
-      let c =  puzzle[a];
-      puzzle[a] = puzzle[b];
-      puzzle[b] = c;
-    }
-    
-    var updateSquare = function(index, num){
-      let t = document.getElementsByClassName(`box-${index}`);
-      t.setState({value: num});
-    }
-    
-    console.log("C is : " + c  + " Puzzle is: " + puzzle[c]);
-    //Check if 0 is bordering it
-    if(puzzle[c]===0){
-      console.log("Sqlocation value is: " + squareLocation[c].sqNum); //Why is this undefined??? SHouldn't it be 1?
-      return 0;
-    }
-    
-    if(c+3<8){
-      if(puzzle[c+3] ===0){
-       swap (c, c+3)
-       updateSquare(c+3, puzzle[c]);
-      console.log("up");
-      console.log("C is : " + c  + " Puzzle is: " + puzzle[c]);
-        return 0;
-      }
-    }
-    if(c-3 >= 0){
-       if(puzzle[c-3] === 0){
-          console.log("down");
-          swap(c, c-3);
-          console.log("C is : " + c  + " Puzzle is: " + puzzle[c]);
-          return 0;
-       }
-    }
-    if(c!= 2 || c!= 5 || c!=8){
-       if(puzzle[c+1] === 0){
-         console.log("right");
-          swap(c,c+1);
-          console.log("C is : " + c  + " Puzzle is: " + puzzle[c]);
-          return 0;
-       }
-    }
-    if(c!==0 || c!= 3 || c!= 6){
-       if(puzzle[c-1]===0){
-       console.log("left");
-       swap (c, c-1);
-       console.log("C is : " + c  + " Puzzle is: " + puzzle[c]);
-        return 0;
-       }
-    }
-    console.log("else");
-    return puzzle[c]; //Return itself
-   }
-  
-  render(){
-    return(
-      <div className={this.props.customClass} /*onClick={() => this.setState({value: this.output(this.props.sqNum)})}*/ onClick={() => this.props.onClick()}>
-        {this.props.num}
+function Square(props){
+  return(
+      <div className={props.customClass} onClick={() => props.onClick()}>
+        {props.num}
       </div>
     );
-  }
 }
 
-class Board extends React.Component{
+function Board(props){
+  let renderSquare = i => {
+    let customClassName = `square col-xs-4 box-${props.squares[i] + 1}`;//could be sm
+    return <Square key={(i+1).toString()} sqNum={i} customClass={customClassName} onClick={() => props.onClick(i)} num={props.squares[i]}/>;
+  };
+    return(
+      <div className="col-xs-8 col-xs-offset-2 col-sm-offset-3 col-md-offset-4">
+        <div className="row">
+          {[...Array(3).keys()].map(i => renderSquare(i))}
+        </div>
+        <div className="row">
+          {[...Array(3).keys()].map(i => renderSquare(i+3))}
+        </div>
+        <div className="row">
+          {[...Array(3).keys()].map(i => renderSquare(i+6))}
+        </div>
+      </div>
+    );
+}
+
+
+class Game extends React.Component {
   constructor() {
     super();
     this.state = {
       squares: shuffle([...Array(9).keys()]),
+      moves: 0
     };
+    this.isOver = false;
+    this.moves = 0;
+  }
+  isSorted(arr){
+    for (let i = 1; i < arr.length; i++)
+      if (arr[i] < arr[i-1]) return false;
+    return true;
   }
   checker(pos, pos_out){
     let y = pos%3;
     let x = Math.trunc(pos/3);
-    console.log("x is " + x);
     //check up
     if ((x - 1 >= 0) && (((x-1)*3 + y) == pos_out)) return true;
     
@@ -119,79 +76,35 @@ class Board extends React.Component{
     
     //check left
     if ((y - 1 >= 0) && ((x*3 + (y-1)) == pos_out)) return true;
-    // for(let i = x-1; i <= x+1; i++){
-    //   for (let j = y-1; j <= y+1; j++){
-    //     if (i > 0 && i < 4 && j > 0 && j < 4){
-    //       let temp_pos = i*3 + j;
-    //       if (temp_pos === pos_out) return true;
-    //     }
-    //   }
-    // }
     return false;
   }
   update(n){
+    if (this.isOver) return;
     let squares = [...this.state.squares];
-    if (squares[n] == 8) return;
+    if (squares[n] === 8) return;
     for (let i = 0; i < squares.length; i++){
-      if (squares[i] == 8 && this.checker(n, i)){
+      if (squares[i] === 8 && this.checker(n, i)){
         squares[i] = squares[n];
         squares[n] = 8;
       }
     }
-    console.log(squares);
-    this.setState({squares: squares});
-    
+    this.moves++;
+    this.setState({squares: squares, moves: this.moves});
   }
-  renderSquare(i){
-    let customClassName = `square col-xs-4 box-${this.state.squares[i] + 1}`;//could be sm
-    let sq = <Square key={(i+1).toString()} sqNum={i} customClass={customClassName} onClick={() => this.update(i)} num={this.state.squares[i]}/>;
-    squareLocation.push(sq);
-    puzzle.push(i);
-    index.push(i);
-    return sq;
-  }
-  render(){
-    return(
-      <div className="col-xs-8 col-xs-offset-2 col-sm-offset-3 col-md-offset-4">
-        <div className="row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        
-        <div className="row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        
-        <div className="row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-
-      </div>
-    );
-  }
-}
-
-
-class Game extends React.Component {
   render() {
     return (
       <div className="container">
         <h1> Cy Sliding Puzzle</h1>
         <div className="status">{status}</div>
         <div className="row">
-          <Board />
+          <Board onClick={(i) => this.update(i)} squares={this.state.squares}/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
           <div className ="col-sm-8 col-sm-offset-2" >
             <div className = "row">
-              <p>Moves: {moves}</p>
+              <p>Moves: {this.state.moves}</p>
               <p>Score: {score}</p>
             </div>
           </div>
