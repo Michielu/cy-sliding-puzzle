a05bd26ca49d0617ff3c8b21cc5dce7441a975e4
Don-V <donv.amupitan@icloud.com>
2017-02-12 18:07:12 -0600

    Added feature to keep score and enhance UI, fixes #1, #2, #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,31 +12,59 @@ var shuffle = arr => {
 };
 
 function Square(props){
+  let bkgImg = (props.bkg) ? {backgroundImage: props.bkg} : {};
   return(
-      <div className={props.customClass} onClick={() => props.onClick()}>
+      <div className={props.customClass} onClick={() => props.onClick()} style ={bkgImg} >
         {props.num}
       </div>
     );
 }
 
 function Board(props){
-  let renderSquare = i => {
-    let customClassName = `square col-xs-4 box-${props.squares[i] + 1}`;//could be sm
-    return <Square key={(i+1).toString()} sqNum={i} customClass={customClassName} onClick={() => props.onClick(i)} num={props.squares[i]}/>;
+  let renderSquare = (i, bkgImg) => {
+    bkgImg = (bkgImg !== undefined && props.squares[i] !== 8) ? bkgImg : false;
+    let customClassName = `square col-xs-4 box-${props.squares[i] + 1}`;
+    return <Square key={(i+1).toString()} customClass={customClassName} onClick={() => props.onClick(i)} num={props.displayNumbers && (props.squares[i] + 1)} bkg={bkgImg}/>;
   };
     return(
       <div className="col-xs-8 col-xs-offset-2 col-sm-offset-3 col-md-offset-4">
         <div className="row">
-          {[...Array(3).keys()].map(i => renderSquare(i))}
+          {[...Array(3).keys()].map(i => renderSquare(i, props.bkg))}
         </div>
         <div className="row">
-          {[...Array(3).keys()].map(i => renderSquare(i+3))}
+          {[...Array(3).keys()].map(i => renderSquare(i+3, props.bkg))}
         </div>
         <div className="row">
-          {[...Array(3).keys()].map(i => renderSquare(i+6))}
+          {[...Array(3).keys()].map(i => renderSquare(i+6, props.bkg))}
         </div>
       </div>
     );
+}
+
+function Options(props){
+    return (
+    <div className="col-xs-5">
+      <button type="button" className="btn" title="reshuffle" onClick={() => props.refreshOnClick()}>
+        <span className="glyphicon glyphicon-refresh"></span>&nbsp;
+      </button>
+      <button type="button" className="btn col-xs-offset-1" title="solve" onClick={() => props.solveOnClick()}>
+        <span className="glyphicon glyphicon-picture"></span>&nbsp;
+      </button>
+      <button type="button" className="btn col-xs-offset-1" title="display numbers" onClick={() => props.toggleNumbersOnClick()}>
+        <span className="glyphicon glyphicon-sunglasses"></span>&nbsp;
+      </button>
+      <div>
+        <br />
+        <p>Select image: </p>
+        <div className="list-group">
+          <p className="list-group-item option" onClick={() => props.pictureOnClick('numbers.png')}>Numbers</p>
+          <p className="list-group-item option" onClick={() => props.pictureOnClick('default.jpg')}>Cy crushing Hawkeye</p>
+          <p className="list-group-item option" onClick={() => props.pictureOnClick('dog.jpg')}>Suprise Suprise</p>
+        </div>
+      </div>
+      
+    </div>
+    )
 }
 
 
@@ -45,16 +73,22 @@ class Game extends React.Component {
     super();
     this.state = {
       squares: shuffle([...Array(9).keys()]),
-      moves: 0
+      moves: 0,
+      background: false,
+      displayNumbers: false
     };
     this.isOver = false;
     this.moves = 0;
+    this.score = 10000;
+    this.startTime = null;
   }
+  
   isSorted(arr){
     for (let i = 1; i < arr.length; i++)
       if (arr[i] < arr[i-1]) return false;
     return true;
   }
+  
   checker(pos, pos_out){
     let y = pos%3;
     let x = Math.trunc(pos/3);
@@ -71,6 +105,7 @@ class Game extends React.Component {
     if ((y - 1 >= 0) && ((x*3 + (y-1)) == pos_out)) return true;
     return false;
   }
+  
   update(n){
     if (this.isOver) return;
     let squares = [...this.state.squares];
@@ -80,28 +115,61 @@ class Game extends React.Component {
         squares[i] = squares[n];
         squares[n] = 8;
         this.moves++;
+        this.score-=2;
       }
+      if (this.moves == 1) this.startTime = (new Date()).getTime()/1000;
     }
-    
+    this.isOver = this.isSorted(squares);
     this.setState({squares: squares, moves: this.moves});
   }
+  
+  changePicture(pic){
+    let newBkg = `url(assets/images/${pic})`;
+    this.setState({background: newBkg});
+  }
+  
+  refreshGame(){
+    this.isOver = false;
+    this.score = 10000;
+    this.moves = 0;
+    this.setState({squares: shuffle([...Array(9).keys()]),
+      moves: 0,
+    });
+  }
+  
+  solveGame(){
+    if (this.isOver) return;
+    this.moves = 0;
+    this.startTime = (new Date()).getTime()/1000;
+    this.setState({squares: [0,1,2,3,4,5,6,8,7],
+        moves: this.moves,
+      });
+  }
+  
+  toggleNumbers(){
+    this.setState({displayNumbers: !this.state.displayNumbers});
+  }
+  
   render() {
     return (
       <div className="container">
         <h1> Cy Sliding Puzzle</h1>
         <div className="status">{status}</div>
         <div className="row">
-          <Board onClick={(i) => this.update(i)} squares={this.state.squares}/>
+          <Board onClick={(i) => this.update(i)} squares={this.state.squares} bkg={this.state.background} displayNumbers={this.state.displayNumbers}/>
         </div>
+        <br />
         <div>
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-          <div className ="col-sm-8 col-sm-offset-2" >
-            <div className = "row info">
-              <p className = "game-info">Moves: {this.state.moves}</p>
-              <p className = "game-info">Score: {}</p>
+          <div className="row">
+            <div className ="col-sm-8 col-sm-offset-2" >
+              <div className = "info">
+                <p className = "game-info">Moves: {this.state.moves}</p>
+                {this.isOver && <p className = "game-info">Score:  {this.score = ((this.score -= Math.floor((this.startTime - (new Date()).getTime()/1000)/10)) < 0) ? 100 : this.score}. You solved it!</p>}
+              </div>
+              <Options pictureOnClick={(i) => this.changePicture(i)} refreshOnClick={() => this.refreshGame()} solveOnClick={() => this.solveGame()} toggleNumbersOnClick={() => this.toggleNumbers()}/>
             </div>
           </div>
+          
         </div>
 
       </div>
